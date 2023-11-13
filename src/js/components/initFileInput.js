f544624b737f17da7ef8952c6ed61840392203f7
vadim <vadim.yakovenko86@gmail.com>
2023-11-14 01:04:57 +0200

    start validation form

--- src/js/components/initFileInput.js
+++ src/js/components/initFileInput.js
@@ -1,14 +1,45 @@
+import {config} from "../utils/config";
+
 export const initFileInput = () => {
   // VARS
   const fileInput = document.querySelector('[data-file-input]');
   if (!fileInput) return;
+  const fileInputName = document.querySelector('[data-file-input-text]');
+  const fileInputError = document.querySelector('[data-file-error]');
 
   // LISTENER
   fileInput.addEventListener('change', function() {
-    const filename = this.files[0].name;
-    const fileInputName = document.querySelector('[data-file-input-text]');
+    const file = this.files[0];
 
-    fileInputName.textContent = filename;
+    if (!file) {
+      fileInputName.textContent = 'Прикріпити файл';
+      hideErrorMessage();
+      return;
+    }
+
+    if (!file.type.startsWith('image/')) {
+      fileInputError.textContent = 'Дозволено завантажувати тільки зображення.';
+      showErrorMessage();
+      return;
+    }
+
+    if (file.size > 5 * 1024 * 1024) {
+      fileInputError.textContent = 'Розмір файлу повинен бути меншим, ніж 5 МБ.';
+      showErrorMessage();
+      return;
+    }
+
+    hideErrorMessage();
+    fileInputName.textContent = file.name;
   });
+
+  // FUNCTIONS
+  function showErrorMessage() {
+    fileInputError.classList.remove(config.hiddenClass);
+  }
+
+  function hideErrorMessage() {
+    fileInputError.classList.add(config.hiddenClass);
+  }
 }
 
